870ab4134b255f0856e67e1b3542895f4bc949f7
Arunkumar00-7 <144720143+Arunkumar00-7@users.noreply.github.com>
2023-10-17 20:36:40 +0530

    Add files via upload

---
+++ Challenge 3.1/main.py
@@ -0,0 +1,13 @@
+def linear_search_product(product_list, target_product):
+  indices = []
+
+  for index, product in enumerate(product_list):
+      if product == target_product:
+          indices.append(index)
+
+  return indices
+
+products = ["apple", "banana", "apple", "orange", "apple", "grape"]
+target = "apple"
+result = linear_search_product(products, target)
+print("Indices of", target, "in the list:", result)
